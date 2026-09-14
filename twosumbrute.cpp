c1766f907607968f85012cec113b9a1cebc7da7c
Soham Ghosh <sgggz217@gmail.com>
2026-09-15 01:16:46 +0530

    two-sum bruteforced
    idk hashmap yet

---
+++ twosumbrute.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        
+        int n = nums.size();
+        int a,b,c=1,d=0,j;
+        int i=0;
+        while (i<=n-2){
+        a=nums[i];
+        b=target-a;
+        j=i+1;
+        while(j<=n-1){
+            if (b==nums[j]){
+                c=j;
+                d=i;
+                break;
+            }else{
+            
+            }
+            j++;
+        }      
+            j=0;
+            i++;
+        }
+        return {d,c};
+    }
+}; 
