912b3988c5b524b54a45a84b4ca91f8865570715
Soham Ghosh <sgggz217@gmail.com>
2026-09-15 01:19:02 +0530

    Update twosumbrute.cpp

--- twosumbrute.cpp
+++ twosumbrute.cpp
@@ -4,14 +4,14 @@ public:
         
         int n = nums.size();
         int a,b,c=1,d=0,j;
-        int i=0;
+        int i=0;            //declaring
         while (i<=n-2){
         a=nums[i];
         b=target-a;
         j=i+1;
         while(j<=n-1){
             if (b==nums[j]){
-                c=j;
+                c=j;                //inner loop for getting indexes of nums if true
                 d=i;
                 break;
             }else{
@@ -19,7 +19,7 @@ public:
             }
             j++;
         }      
-            j=0;
+            j=0;    //resetting the inner loop
             i++;
         }
         return {d,c};
